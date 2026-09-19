1482f6e94fc76d81b274ebc90be663be2f6a27e3
appbuilderlee2 <appbuilderlee2@gmail.com>
2026-09-19 18:34:06 +0930

    Update writeback error text for hex Tj support

--- Sources/PDFEngine/PDFContentEngine.swift
+++ Sources/PDFEngine/PDFContentEngine.swift
@@ -362,7 +362,7 @@ public final class PDFContentEngine {
                 )
             case .unsupportedEncoding:
                 throw PDFContentError.notYetImplemented(
-                    "目前 writeback milestone 只支援 printable ASCII 的 literal Tj/TJ"
+                    "目前 writeback milestone 支援 printable ASCII 的 literal Tj/TJ 及 byte-oriented hex Tj"
                 )
             case .unsupportedFilter:
                 throw PDFContentError.notYetImplemented(
